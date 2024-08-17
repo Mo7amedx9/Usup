8b74a76bfa6cdade3769aead6d34cb1efd6aae3a
Mo7amedx9 <mohamedreda889900m@gmail.com>
2024-08-17 22:29:45 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, render_template, request, send_from_directory
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def index():
+    images = os.listdir(UPLOAD_FOLDER)
+    return render_template('gallery.html', images=images)
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return 'No file part'
+    file = request.files['image']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filepath)
+        return 'Image received', 200
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)  # User site running on port 5001
